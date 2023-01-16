23503ab7572b9c65f673571ee9a9eac0e0e7954b
Jordan Irwin <antumdeluge@gmail.com>
2023-01-16 04:38:03 -0800

    Clode cleanup

--- main.js
+++ main.js
@@ -149,78 +149,79 @@ function cropImage(img) {
   return new Image(dataUrl);
 }
 
-  let ce = document.createElement.bind(document);
+const ce = document.createElement.bind(document);
 
-  function cext(tag, props) {
-      props = props || {};
-      const e = ce (tag);
-      for (var s in props){
-          e[s] = props[s];
-      }
-      return e;
+function cext(tag, props) {
+  props = props || {};
+  const e = ce (tag);
+  for (var s in props){
+    e[s] = props[s];
   }
+  return e;
+}
 
-  let append = function (what,where) {
-    where = where || document.body;
-    where.appendChild(what);
-  };
+function append(what, where) {
+  where = where || document.body;
+  where.appendChild(what);
+};
 
-async function init(){
-  let title = cext("div", {id:"title", innerHTML: "Stendhal " + version.join(".")});
+async function init() {
+  const title = cext("div", {id: "title", innerHTML: "Stendhal " + version.join(".")});
   append(title);
-  let typesContainer = cext("div",{id :"container"});
+  const typesContainer = cext("div", {id: "container"});
   append(typesContainer);
-  let icons = cext("div",{id:"images"});
+  const icons = cext("div", {id: "images"});
 
-  let selectType = ce("select");
-  let dummyoption = cext("option", {
-      value : -1,
-      textContent : "--select one--",
+  const selectType = ce("select");
+  const dummyoption = cext("option", {
+    value: -1,
+    textContent: "--select one--"
   });
-  append(dummyoption,selectType);
-  append(selectType,typesContainer);
-  const showIcons = (cext("input",{
-      value:"showicons",
-      type:"button",
-      onclick: function () {
-          icons.style.display = '';
-      },
-  }));
+  append(dummyoption, selectType);
+  append(selectType, typesContainer);
+
+  const showIcons = cext("input", {
+    value: "showicons",
+    type: "button",
+    onclick: function() {
+      icons.style.display = '';
+    }
+  });
   showIcons.style.display = "none";
-  append(showIcons,typesContainer);
-  append(icons,typesContainer);
+  append(showIcons, typesContainer);
+  append(icons, typesContainer);
   for (const itype of Object.keys(types)) {
     const iname = types[itype];
     if (iname) {
-      let iconContainer = cext("div");
-      let caption = cext("span", {textContent: itype});
+      const iconContainer = cext("div");
+      const caption = cext("span", {textContent: itype});
       const img = getImage(singularOf(itype), iname);
 
-      append(img,iconContainer);
-      append(caption,iconContainer);
+      append(img, iconContainer);
+      append(caption, iconContainer);
 
-      append(iconContainer,icons);
+      append(iconContainer, icons);
       iconContainer.addEventListener('click', function (e) {
-             selectType.value = e.currentTarget.getElementsByTagName("span")[0].textContent;
-             icons.style.display  = "none";
-             showIcons.style.display = "";
-             typeSelectChange({target:selectType});
+        selectType.value = e.currentTarget.getElementsByTagName("span")[0].textContent;
+        icons.style.display  = "none";
+        showIcons.style.display = "";
+        typeSelectChange({target:selectType});
       });
     }
 
-      let option = cext("option",{
-        textContent: itype,
-        value: itype,
+    let option = cext("option", {
+      textContent: itype,
+      value: itype
     });
-    append(option,selectType);
+    append(option, selectType);
   }
 
   selectType.autocomplete = "off";
   selectType.addEventListener("change", typeSelectChange);
-  let items = cext("div",{
-      id:"items",
+  const items = cext("div", {
+    id:"items"
   });
-  append(items,typesContainer);
+  append(items, typesContainer);
 }
 
 var oldSortColumn = -1;
@@ -228,83 +229,83 @@ var sortOrder = "asc";
 let sortColumnIndex = 0;
 
 function sortBy(e) {
-    let target = e.target;
-    let tr = target.parentNode;
-    for (var i = 0,n = tr.childNodes.length;i<n;++i){
-        if (tr.childNodes[i] == target){
-          sortColumnIndex = i;
-          break;
-        }
+  const target = e.target;
+  const tr = target.parentNode;
+  for (var i = 0, n = tr.childNodes.length; i < n; ++i) {
+    if (tr.childNodes[i] == target){
+      sortColumnIndex = i;
+      break;
     }
+  }
 
-    let table = document.getElementsByTagName("table")[0];
-    let rows = table.getElementsByTagName("tr");
+  const table = document.getElementsByTagName("table")[0];
+  let rows = table.getElementsByTagName("tr");
 
-    rows = Array.prototype.slice.call(rows,1);
-    for (var i = 0;i<rows.length;++i){
-        table.removeChild(rows[i]);
-    }
+  rows = Array.prototype.slice.call(rows, 1);
+  for (var i = 0; i < rows.length; ++i) {
+    table.removeChild(rows[i]);
+  }
 
-    if (oldSortColumn == sortColumnIndex) {
-        if (sortOrder == "asc") {
-            sortOrder = "desc";
-        }else{
-            sortOrder = "asc";
-        }
-    }else {
+  if (oldSortColumn == sortColumnIndex) {
+    if (sortOrder == "asc") {
+      sortOrder = "desc";
+    } else {
       sortOrder = "asc";
     }
+  } else {
+    sortOrder = "asc";
+  }
 
-    var comparator = function (a,b) {
-        a = a.getElementsByTagName("td")[sortColumnIndex].textContent;
-        b = b.getElementsByTagName("td")[sortColumnIndex].textContent;
-        let ia = parseFloat(a);
-        let ib = parseFloat(b);
-        let r = 0;
-        if (!isNaN(ia)){
-            if (!isNaN(ib)){
-                r = ia - ib;
-            }else{
-                ib = 0;
-                r = ia - ib;
-            }
-        }else {
-            if (!isNaN(ib)){
-                ia = 0;
-                r = ia - ib;
-            }else{
-                r = (""+a).localeCompare(""+b);
-            }
-        }
-        if (sortOrder != "asc") {
-            r = -1 * r;
-        }
-        return r;
+  function comparator(a, b) {
+    a = a.getElementsByTagName("td")[sortColumnIndex].textContent;
+    b = b.getElementsByTagName("td")[sortColumnIndex].textContent;
+    let ia = parseFloat(a);
+    let ib = parseFloat(b);
+    let r = 0;
+    if (!isNaN(ia)) {
+      if (!isNaN(ib)) {
+        r = ia - ib;
+      } else {
+        ib = 0;
+        r = ia - ib;
+      }
+    } else {
+      if (!isNaN(ib)) {
+        ia = 0;
+        r = ia - ib;
+      } else {
+        r = (""+a).localeCompare(""+b);
+      }
     }
+    if (sortOrder != "asc") {
+      r = -1 * r;
+    }
+    return r;
+  }
 
-    Array.prototype.sort.call(rows, comparator);
-    for (var i = 0;i<rows.length;++i){
-        table.appendChild(rows[i]);
-    }
-    oldSortColumn = sortColumnIndex;
+  Array.prototype.sort.call(rows, comparator);
+  for (var i = 0; i < rows.length; ++i) {
+    table.appendChild(rows[i]);
+  }
+  oldSortColumn = sortColumnIndex;
 }
 
 function parseResistances(allAttributes, attributes, item, type) {
-    let resistances = item.getElementsByTagName(type);
-    for (var j = 0;j<resistances.length;++j){
-        let attributeName = resistances[j].attributes[0].nodeValue;
-        let value = resistances[j].attributes[1].nodeValue;
-        allAttributes[attributes[attributeName]] = value;
-    }
-    return allAttributes;
+  const resistances = item.getElementsByTagName(type);
+  for (var j = 0; j < resistances.length; ++j) {
+    const attributeName = resistances[j].attributes[0].nodeValue;
+    const value = resistances[j].attributes[1].nodeValue;
+    allAttributes[attributes[attributeName]] = value;
+  }
+  return allAttributes;
 }
 
-async function fetchXML(url){
-    const response = await fetch(url);
-    const xml = await response.text();
-    const parser = new DOMParser();
-    const xmlDoc  = parser.parseFromString(xml,'text/xml');
-    return xmlDoc;
+async function fetchXML(url) {
+  const response = await fetch(url);
+  const xml = await response.text();
+  const parser = new DOMParser();
+  const xmlDoc  = parser.parseFromString(xml, "text/xml");
+  return xmlDoc;
 }
 
 async function typeSelectChange(e) {
@@ -326,84 +327,82 @@ async function typeSelectChange(e) {
         + escape(version[1]) + "/data/conf/items/"
         + escape(e.target.value) + ".xml";
     const xmlDoc  = await fetchXML(url);
-    let itemsDiv = document.getElementById("items");
+    const itemsDiv = document.getElementById("items");
     itemsDiv.textContent = "";
-    let attributes = {};
+    const attributes = {};
     let attributeCount = 1;
-    let items = xmlDoc.getElementsByTagName("item");
-    for (i = 0;i<items.length;++i){
-        const item = items[i];
+    const items = xmlDoc.getElementsByTagName("item");
+    for (i = 0; i < items.length; ++i) {
+      const item = items[i];
       let itemAttributes = item.getElementsByTagName("attributes");
-      if (itemAttributes.length == 1){
+      if (itemAttributes.length == 1) {
         itemAttributes = itemAttributes[0];
-        for (let j = 0;j<itemAttributes.children.length;++j){
-            const attName = itemAttributes.children[j].nodeName;
-            if (!excluded[attName]) {
-              attributes[attName] = attributes[itemAttributes.children[j].nodeName] || attributeCount++;
-            }
+        for (let j = 0; j < itemAttributes.children.length; ++j) {
+          const attName = itemAttributes.children[j].nodeName;
+          if (!excluded[attName]) {
+            attributes[attName] = attributes[itemAttributes.children[j].nodeName] || attributeCount++;
+          }
         }
-
       }
-      const fillAttributes = function (source){
-          return function () {
-              const elements = item.getElementsByTagName(source);
-              for (j = 0;j<elements.length;++j){
-                  attributes[elements[j].attributes[0].nodeValue] = attributes[elements[j].attributes[0].nodeValue] || attributeCount++;
-              }
-          };
+
+      function fillAttributes(source) {
+        return function() {
+          const elements = item.getElementsByTagName(source);
+          for (j = 0; j < elements.length; ++j) {
+            attributes[elements[j].attributes[0].nodeValue] = attributes[elements[j].attributes[0].nodeValue] || attributeCount++;
+          }
+        };
       };
 
       fillAttributes("resistance")();
       fillAttributes("susceptibility")();
-
     }
 
-    let table = ce("table");
-    let thead = ce("thead");
+    const table = ce("table");
+    const thead = ce("thead");
     let tr = ce("tr");
     let td = ce("th");
 
-    append(thead,table);
-    append(tr,thead);
+    append(thead, table);
+    append(tr, thead);
 
     td.textContent = "image";
-    append(td,tr);
+    append(td, tr);
 
-    td = cext("th",{
+    td = cext("th", {
       textContent: "name"
     });
     td.classList.add("sortable");
     td.addEventListener("click", sortBy);
-    append(td,tr);
+    append(td, tr);
 
-    for (var i in attributes){
-      td = cext("th",{
-          textContent:i,
+    for (var i in attributes) {
+      td = cext("th", {
+          textContent: i
       });
       td.classList.add("sortable");
-      append(td,tr);
+      append(td, tr);
       td.addEventListener("click", sortBy);
     }
 
-    for (var i = 0;i<items.length;++i){
+    for (var i = 0; i < items.length; ++i) {
       const item = items[i];
       let unattainable = false;
 
       let itemAttributes = item.getElementsByTagName("attributes");
       let allAttributes = [];
 
-      if (itemAttributes.length==1){
+      if (itemAttributes.length == 1) {
         itemAttributes = itemAttributes[0];
-        for (j = 0;j<itemAttributes.children.length;++j){
-            const child = itemAttributes.children[j];
-            if (child.tagName === "unattainable") {
-              unattainable = child.innerHTML === "true";
-            }
-            let attributeName = child.nodeName;
-            if (child.attributes.value) {
-              allAttributes[attributes[attributeName]] = child.attributes.value.nodeValue;
-            }
+        for (j = 0; j < itemAttributes.children.length; ++j) {
+          const child = itemAttributes.children[j];
+          if (child.tagName === "unattainable") {
+            unattainable = child.innerHTML === "true";
           }
+          if (child.attributes.value) {
+            allAttributes[attributes[child.nodeName]] = child.attributes.value.nodeValue;
+          }
+        }
       }
 
       if (unattainable) {
@@ -413,9 +412,8 @@ async function typeSelectChange(e) {
 
       let tr = ce("tr");
       let td = ce("td");
-      append(td,tr);
+      append(td, tr);
       let type = item.getElementsByTagName("type")[0];
-
 
       const homepage = getHomeUrl(escape(type.attributes[0].nodeValue), escape(item.attributes[0].value));
 
@@ -423,26 +421,25 @@ async function typeSelectChange(e) {
       append(getImage(escape(type.attributes[0].nodeValue), escape(type.attributes[1].value)), td);
 
       td = ce("td");
-      append(td,tr);
-      append(cext('a', {
-          href: homepage,
-          target: "_blank",
-          textContent: item.attributes[0].value,
-      }),td);
+      append(td, tr);
+      append(cext("a", {
+        href: homepage,
+        target: "_blank",
+        textContent: item.attributes[0].value
+      }), td);
 
-      append(tr,table);
+      append(tr, table);
 
-      allAttributes = parseResistances(allAttributes,attributes,items[i],"resistance");
-      allAttributes = parseResistances(allAttributes,attributes,items[i],"susceptibility");
+      allAttributes = parseResistances(allAttributes, attributes, items[i], "resistance");
+      allAttributes = parseResistances(allAttributes, attributes, items[i], "susceptibility");
 
-
-        for (var j = 1; j <= attributeCount-1; ++j) {
-            append(cext("td",{
-                textContent: allAttributes[j] || "",
-            }),tr);
-        }
+      for (var j = 1; j <= attributeCount-1; ++j) {
+        append(cext("td", {
+          textContent: allAttributes[j] || ""
+        }), tr);
+      }
     }
-    append(table,itemsDiv);
+    append(table, itemsDiv);
   } catch (ex) {
     console.log(ex);
   }
